981c52f0464c2582eea1f7f0486725aa8342b565
intruder <getngothonow@gmail.com>
2014-05-19 15:12:38 +0300

    views edit

--- polygonapp/views.py
+++ polygonapp/views.py
@@ -6,36 +6,40 @@ from .models import Region
 from django.contrib.gis.geos import (Point, fromstr, fromfile, 
                 GEOSGeometry, MultiPoint, MultiPolygon, Polygon)
 import json
-
-#def index(request):
+from django.core import serializers
 from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
 def index(request):
-
+	'''
+	Here we'll get the points from the user input, and store them
+	in a PolygonField. Also, title and description of the polygon
+	'''
 	if request.method == 'POST':
 		s = request.POST.get('data')
-		print s
 		d = json.loads(s)['overlays'][0]
 		title = d['title']
 		desc = d['content']
-		print tuple(d['paths'][0])
+		c = d['paths'][0]#here we get the key-value pair of lat/long of all the points
+		cord_list = []
+		coords = [[v for k, v in d.iteritems() ] for d in c]#remove the keys from the dict, make it a nested list
 
-
-		coords = request.POST
-		#cord_list = []
-		#for p in coords:
-		#	print p[0]
-		#	cord_list.append( GEOSGeometry('POINT(%s %s)' %(p[0], p[1])))
-		#	print cord_list
+		if coords[0] != coords[-1]:#if it's not a linear ring, force it---> mischevous, sorry.
+			coords.append(coords[0])
+		for p in coords:
+			cord_list.append(GEOSGeometry('POINT(%s %s)' %(p[0], p[1])))
+		p = Polygon(cord_list)
+		region_obj = Region(name=title, mpolygon=p)
+		region_obj.save()
 	return render_to_response('test5.html', {},
                           context_instance=RequestContext(request))
 
 
 
 def region(request):
-	region  = Region.objects.all().order_by('-id')[0]
-
-	return render_to_response('maps.html', {
-        'obj': region,
-    }, context_instance=RequestContext(request))
+	pol_obj = Region.objects.all().order_by('-id')[0]
+	geojson = pol_obj.mpolygon.geojson
+	coords =  pol_obj.mpolygon.coords[0]
+	centre =  pol_obj.mpolygon.centroid.coords
+	return render_to_response('draw.html', 
+		{'geojson': geojson,'coords':coords, 'centroid': centre, }, context_instance=RequestContext(request))
